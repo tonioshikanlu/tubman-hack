9fbc4eabe0fa88d33b1ba83075b82bb533493a36
Aimie Ojuba <41306649+Cytla24@users.noreply.github.com>
2021-03-20 12:32:41 -0400

    Added constants

---
+++ consts.py
@@ -0,0 +1,15 @@
+
+# Alpaca API URLs Endpoints
+BASE_URL = "https://paper-api.alpaca.markets"
+DATA_URL = "https://data.alpaca.markets/v1"
+
+# How far back we want to request historical data for in days
+MAX_HISTORY = 50
+HISTORY_RANGE = 'day'
+
+CURRENT_STOCK_RANGE = 'minute'
+
+# Documentation for alpaca API
+# https://alpaca.markets/docs/api-documentation/api-v2/
+# https://github.com/alpacahq/alpaca-trade-api-python
+
